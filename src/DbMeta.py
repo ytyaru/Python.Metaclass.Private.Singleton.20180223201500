f4e35fdca040009fecccb5bb621a29e3995d8273
ytyaru <ytyaru@outlook.jp>
2018-02-23 20:34:41 +0900

    呼出確認メッセージ出力

--- src/DbMeta.py
+++ src/DbMeta.py
@@ -1,14 +1,18 @@
 class DbMeta(type):
     def __new__(cls, name, bases, attrs):
+        print('__new__')
         attrs['_{0}__secret'.format(name)] = 'my_secret' # Db.__secret 定義
         return type.__new__(cls, name, bases, attrs)
 
     def __init__(cls, name, bases, attrs):
+        print('__init__')
         setattr(cls, 'Secret', property(lambda cls: attrs['_{0}__secret'.format(name)])) # Db.Secretプロパティ定義
 
     _instance = None
     def __call__(cls, *args, **kwargs):
+        print('__call__')
         if cls._instance is None:
             cls._instance = super().__call__(*args, **kwargs)
+            print('***** singleton instance *****')
         return cls._instance
 
